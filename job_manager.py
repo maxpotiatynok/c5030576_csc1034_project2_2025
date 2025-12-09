147403cd3d3c3b00aa428c59168d34c1425ac84f
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-09 21:58:05 +0000

    Add JobManager class

---
+++ job_manager.py
@@ -0,0 +1,43 @@
+class JobManager:
+
+    def __init__(self, jobs=None):
+        pass
+
+    def get_jobs(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def add_job(self, job):
+        pass
+
+    def remove_job(self, job):
+        pass
+
+    def edit_job(self, old_job, new_job):
+        pass
+
+    def search_by_category(self, category):
+        pass
+
+    def search_by_rate(self, rate):
+        pass
+
+    def search_by_name_and_date(self, name, date):
+        pass
+
+    def get_total_cost_per_name(self, names):
+        pass
+
+    def get_category_count_per_name(self):
+        pass
+
+    def load_from_file(self, file_name):
+        pass
+
+    def save_to_file(self, file_name):
+        pass
