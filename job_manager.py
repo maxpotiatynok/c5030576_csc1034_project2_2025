47b06f4d8aef77f776fcbfb34b3cf376a8b559cd
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-11 05:33:49 +0000

    Add more comments to the JobManager class

--- job_manager.py
+++ job_manager.py
@@ -10,6 +10,7 @@ class JobManager:
         else:
             self.__jobs = jobs
 
+    # Get the list of jobs
     def get_jobs(self):
         return self.__jobs
 
@@ -21,16 +22,18 @@ class JobManager:
     def __repr__(self):
         return f"Job manager({self.__jobs})"
 
+    # Add a new job to the list
     def add_job(self, job):
-        if job not in self.__jobs:
+        if job not in self.__jobs: # Check if a similar job already exists in the list
             workers_hours = 0
             for j in self.__jobs:
                 # Check if such job already exists
                 if j.getname() == job.getname():
                     workers_hours += j.get_hours()
         else:
-            Exception('Job already exists')
+            Exception('Job already exists') # Throw an exception if the conditions above aren't met
 
+    # Remove a job from the list
     def remove_job(self, job):
         if job in self.__jobs:
             self.__jobs.remove(job)
@@ -43,13 +46,16 @@ class JobManager:
             self.__jobs.remove_job(old_job)
             self.__jobs.add_job(new_job)
 
+    # Search jobs by a category
     def search_by_category(self, category):
         suitable_jobs = []
         for job in self.__jobs:
             if job.getcategory() == category:
                 suitable_jobs.append(job)
-        return suitable_jobs
+        return suitable_jobs # Returns all matching jobs
 
+
+    # Search jobs by rate
     def search_by_rate(self, rate):
         suitable_jobs = []
         for job in self.__jobs:
@@ -57,13 +63,15 @@ class JobManager:
                 suitable_jobs.append(job)
         return suitable_jobs
 
+    # Search jobs by name and date
     def search_by_name_and_date(self, name, date):
         suitable_jobs = []
         for job in self.__jobs:
-            if job.getname() == name and job.getdate() == date:
+            if job.getname() == name and job.getdate() == date: # Search jobs that exactly match name and date requirements
                 suitable_jobs.append(job)
         return suitable_jobs
 
+    # Return a dictionary that maps each name to the total cost of all jobs under this name
     def get_total_cost_per_name(self, names):
         name_cost_dict = {}
         for job in self.__jobs:
@@ -77,6 +85,7 @@ class JobManager:
                     name_cost_dict[job.getname()] += job.get_rate() * job.get_hours()
         return name_cost_dict
 
+    # Return a dictionary that maps name to dictionary that maps each category to the number of jobs of that category
     def get_category_count_per_name(self):
         cat_c_n_dict = {}
         for job in self.__jobs:
@@ -91,8 +100,10 @@ class JobManager:
                 cat_c_n_dict[job.category] = cat_j_dict[job.category]
         return cat_c_n_dict
 
+    # Read a file and add jobs from the file to the jobs list
     def load_from_file(self, file_name):
         pass
 
+    # Write jobs from the jobs list to a file
     def save_to_file(self, file_name):
         pass
