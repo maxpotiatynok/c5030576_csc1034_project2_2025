d52255b13add36819fe3120ec237ded701b90255
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-11 05:36:23 +0000

    Add more comments to the Job class

--- job.py
+++ job.py
@@ -1,29 +1,35 @@
 class Job:
+    # Constructor
     def __init__(self, name, category, rate, date, hours):
         self.__name = name
         self.__category = category
         if rate > 0: # Should be positive
             self.__rate = rate
         else:
-            Exception('rate must be positive')
+            Exception('rate must be positive') # Throw an exception if the rate is negative or zero
         self.__date = date
         if 0 < hours <= 6: # Should be positive and no more than 6
             self.__hours = hours
         else:
-            Exception('hours must be between 0 and 6')
+            Exception('hours must be between 0 and 6') # Throw an exception if the number of hours is more than 6
 
+    # Get name
     def get_name(self):
         return self.__name
 
+    # Get category
     def get_category(self):
         return self.__category
 
+    # Get rate
     def get_rate(self):
         return self.__rate
 
+    # Get date
     def get_date(self):
         return self.__date
 
+    # Get hours
     def get_hours(self):
         return self.__hours
 
