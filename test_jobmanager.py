aa729d8da9c92499f2ebd535451d680569b32c3e
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-12 10:09:27 +0000

    Create method to test get_jobs method of the jobmanager class

--- test_jobmanager.py
+++ test_jobmanager.py
@@ -14,3 +14,11 @@ class TestJobManager:
         """Tests the constructor by not using a list as a parameter"""
         job_manager = JobManager()
         assert isinstance(job_manager, JobManager)
+
+    def test_get_job(self):
+        """Tests the get_job function"""
+        job1 = Job("John Brown", "Technical", 13.45, "21/10/2026", 4)
+        job2 = Job("Vladimir Kuznetsov", "Human Resources", 14.10, "24/10/2026", 6)
+        job_list = [job1, job2]
+        job_manager = JobManager(job_list)
+        assert job_manager.get_jobs() ==[job1, job2]
