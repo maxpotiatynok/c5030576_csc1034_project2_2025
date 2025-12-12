0c26504786124ee98a936ca6144847c02ea1f1de
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-12 09:28:03 +0000

    Replace some comments with docstrings

--- job_manager.py
+++ job_manager.py
@@ -1,34 +1,31 @@
 import csv
-
 from job import Job
 
-
 class JobManager:
-    import csv
-    # Constructor
+    """Constructor"""
     def __init__(self, jobs=None):
-        # jobs is an optional parameter
-        # meaning it can be null
-        # if so, the list of jobs will be empty
+        """jobs is an optional parameter
+         meaning it can be null
+        if so, the list of jobs will be empty"""
         if jobs is None:
             self.__jobs = []
         else:
             self.__jobs = jobs
 
-    # Get the list of jobs
     def get_jobs(self):
+        """Get the list of jobs"""
         return self.__jobs
 
-    # String representation
-    def __str__(self): #
+    def __str__(self):
+        """String representation"""
         return f"Job manager: {self.__jobs}"
 
-    # Formal string representation for developers
     def __repr__(self):
+        """Formal string representation for developers"""
         return f"Job manager({self.__jobs})"
 
-    # Add a new job to the list
     def add_job(self, job):
+        """Add a job to the list of jobs"""
         if job not in self.__jobs: # Check if a similar job already exists in the list
             workers_hours = 0
             for j in self.__jobs:
@@ -36,23 +33,23 @@ class JobManager:
                 if j.getname() == job.getname():
                     workers_hours += j.get_hours()
         else:
-            Exception('Job already exists') # Throw an exception if the conditions above aren't met
+            raise Exception('Job already exists') # Throw an exception if the conditions above aren't met
 
-    # Remove a job from the list
     def remove_job(self, job):
+        """Remove a job from the list of jobs"""
         if job in self.__jobs:
             self.__jobs.remove(job)
         else:
-            Exception('Job does not exist')
+            raise Exception('Job does not exist')
 
-    # Replace old job with the new one
     def edit_job(self, old_job, new_job):
+        """Edit a job in the list of jobs"""
         if old_job in self.__jobs:
             self.__jobs.remove_job(old_job)
             self.__jobs.add_job(new_job)
 
-    # Search jobs by a category
     def search_by_category(self, category):
+        """Search for jobs with the given category"""
         suitable_jobs = []
         for job in self.__jobs:
             if job.getcategory() == category:
@@ -60,24 +57,24 @@ class JobManager:
         return suitable_jobs # Return all matching jobs
 
 
-    # Search jobs by rate
     def search_by_rate(self, rate):
+        """Search for jobs with the given rate"""
         suitable_jobs = []
         for job in self.__jobs:
             if job.getrate() == rate:
                 suitable_jobs.append(job)
         return suitable_jobs
 
-    # Search jobs by name and date
     def search_by_name_and_date(self, name, date):
+        """Search for jobs with the given name and date"""
         suitable_jobs = []
         for job in self.__jobs:
             if job.getname() == name and job.getdate() == date: # Search jobs that exactly match name and date requirements
                 suitable_jobs.append(job)
         return suitable_jobs
 
-    # Return a dictionary that maps each name to the total cost of all jobs under this name
     def get_total_cost_per_name(self, names):
+        """Return a dictionary that maps each name to the total cost of all jobs under this name"""
         name_cost_dict = {}
         for job in self.__jobs:
             if job.getname() in names:
@@ -86,12 +83,12 @@ class JobManager:
                     # if not - create a new key-value pair
                     name_cost_dict[job.getname()] = job.get_rate() * job.get_hours()
                 else:
-                    #if the pair exists, add job cost to the total cost
+                    # if the pair exists, add job cost to the total cost
                     name_cost_dict[job.getname()] += job.get_rate() * job.get_hours()
         return name_cost_dict
 
-    # Return a dictionary that maps name to dictionary that maps each category to the number of jobs of that category
     def get_category_count_per_name(self):
+        """Return a dictionary that maps name to dictionary that maps each category to the number of jobs of that category"""
         cat_c_n_dict = {}
         for job in self.__jobs:
             if job.name not in cat_c_n_dict:
@@ -105,8 +102,8 @@ class JobManager:
                 cat_c_n_dict[job.category] = cat_j_dict[job.category]
         return cat_c_n_dict
 
-    # Read a file and add jobs from the file to the jobs list
     def load_from_file(self, file_name):
+        """Reads a file and adds jobs from the file to the jobs list"""
         with open(file_name, "r", newline=' ') as csv_file: # Read each row from a file
             reader = csv.reader(csv_file, delimiter=',', quoting=csv.QUOTE_NONE) # Separate columns with comma and without quotation marks
             next(reader) # Skip the first row
@@ -114,10 +111,10 @@ class JobManager:
                 if row != [] and len(row) == 5: # The row should not be empty and have less than 5 columns
                     self.add_job(Job(row[0], row[1], row[2], row[3], row[4])) # create a job object with parameters from the file
                 else:
-                    Exception('Invalid row')
+                    raise Exception('Invalid row')
 
-    # Write jobs from the jobs list to a file
     def save_to_file(self, file_name):
+        """Write jobs from the jobs list to a file"""
         list_count = 0 # Count each job object in the jobs list
         with open(file_name, "w", newline=' ') as csv_file: # Write each row into a file
             writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_NONE)
