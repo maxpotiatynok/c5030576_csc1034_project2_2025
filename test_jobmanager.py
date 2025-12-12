1fdcb47a479cc8e038c0b1b61e5096f4d420264f
maxpotiatynok <c5030576@newcastle.ac.uk>
2025-12-12 09:59:13 +0000

    Create class to test jobmanager class; create methods to test the constructor

---
+++ test_jobmanager.py
@@ -0,0 +1,16 @@
+import pytest
+from job_manager import JobManager
+from job import Job
+class TestJobManager:
+    def test_init_full(self):
+        """Tests the constructor by using a list as a parameter"""
+        job1 = Job("John Brown", "Technical", 13.45, "21/10/2026", 4)
+        job2 = Job("Vladimir Kuznetsov", "Human Resources", 14.10, "24/10/2026", 6)
+        job_list = [job1, job2]
+        job_manager = JobManager(job_list)
+        assert isinstance(job_manager, JobManager)
+
+    def test_init_empty(self):
+        """Tests the constructor by not using a list as a parameter"""
+        job_manager = JobManager()
+        assert isinstance(job_manager, JobManager)
